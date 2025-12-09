c64bc7072fef7341ac854c8319cf7356a6dba328
atucker2346 <anthony.tucker@bison.howard.edu>
2025-12-09 01:00:52 -0500

    Scoreboard Fix

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.questions import questions_bp
-from routes.scoreboard import scoreboard_bp
+from routes.scoreboard.Scoreboard import scoreboard_bp
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(questions_bp)
 app.register_blueprint(scoreboard_bp)
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
